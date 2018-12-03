8c2437e42e76edaa0d61daa712e6ca8185b61c1c
JonyPipa <36369025+JonyPipa@users.noreply.github.com>
2018-12-03 18:11:43 +0100

    Add files via upload

---
+++ diccreator.py
@@ -0,0 +1,153 @@
+dic = open("dicc.txt", "a")
+words = ["1","2","3","4"]
+n1 = 0
+n2 = 0
+ll = ""
+for i in words:
+	for i in words:
+		ll = (words[n1] + words[n2] + "\n")
+		dic.write(ll)
+		n2 += 1
+	n1 += 1
+	n2 = 0
+
+n1 = 0
+n2 = 0
+n3 = 0
+for i in words:
+	for i in words:
+		for i in words:
+			ll = (words[n1] + words[n2] + words[n3] + "\n")
+			dic.write(ll)
+			n3 += 1
+		n2 += 1
+		n3 = 0
+	n1 += 1
+	n2 = 0
+	n3 = 0
+		
+
+n1 = 0
+n2 = 0
+n3 = 0
+for i in words:
+	for i in words:
+		for i in words:
+			ll = (str.capitalize(words[n1]) + words[n2] + words[n3] + "\n")
+			dic.write(ll)
+			n3 += 1
+		n2 += 1
+		n3 = 0
+	n1 += 1
+	n2 = 0
+	n3 = 0
+
+
+n1 = 0
+n2 = 0
+n3 = 0
+for i in words:
+	for i in words:
+		for i in words:
+			ll = (words[n1] + str.capitalize(words[n2]) + words[n3] + "\n")
+			dic.write(ll)
+			n3 += 1
+		n2 += 1
+		n3 = 0
+	n1 += 1
+	n2 = 0
+	n3 = 0
+
+n1 = 0
+n2 = 0
+n3 = 0
+for i in words:
+	for i in words:
+		for i in words:
+			ll = (words[n1] + words[n2] + str.capitalize(words[n3]) + "\n")
+			dic.write(ll)
+			n3 += 1
+		n2 += 1
+		n3 = 0
+	n1 += 1
+	n2 = 0
+	n3 = 0
+
+n1 = 0
+n2 = 0
+n3 = 0
+for i in words:
+	for i in words:
+		for i in words:
+			ll = (words[n1] + str.capitalize(words[n2]) + str.capitalize(words[n3]) + "\n")
+			dic.write(ll)
+			n3 += 1
+		n2 += 1
+		n3 = 0
+	n1 += 1
+	n2 = 0
+	n3 = 0
+
+n1 = 0
+n2 = 0
+n3 = 0
+for i in words:
+	for i in words:
+		for i in words:
+			ll = (str.capitalize(words[n1]) + words[n2] + str.capitalize(words[n3]) + "\n")
+			dic.write(ll)
+			n3 += 1
+		n2 += 1
+		n3 = 0
+	n1 += 1
+	n2 = 0
+	n3 = 0
+
+n1 = 0
+n2 = 0
+n3 = 0
+for i in words:
+	for i in words:
+		for i in words:
+			ll = (str.capitalize(words[n1]) + str.capitalize(words[n2]) + words[n3] + "\n")
+			dic.write(ll)
+			n3 += 1
+		n2 += 1
+		n3 = 0
+	n1 += 1
+	n2 = 0
+	n3 = 0
+
+n1 = 0
+n2 = 0
+n3 = 0
+for i in words:
+	for i in words:
+		for i in words:
+			ll = (str.capitalize(words[n1]) + str.capitalize(words[n2]) + str.capitalize(words[n3]) + "\n")
+			dic.write(ll)
+			n3 += 1
+		n2 += 1
+		n3 = 0
+	n1 += 1
+	n2 = 0
+	n3 = 0
+	
+
+
+
+
+
+
+dic.close()
+
+
+
+
+
+
+
+
+
+
+
